dea63c296600bbdb943d502a8ccd18da5616324d
Mohammad Othman <emranm226@hotmail.com>
2025-08-30 00:49:32 +0300

    feat: register UN scraper in scrapers package

--- backend/src/scrapers/__init__.py
+++ backend/src/scrapers/__init__.py
@@ -7,5 +7,6 @@ from src.scrapers.registry import scraper_registry
 
 # Import all scraper modules to trigger registration
 from src.scrapers.us.ofac.scraper import OFACScraper
+from src.scrapers.international.un.scraper import UNScraper
 
-__all__ = ['scraper_registry', 'OFACScraper']
+__all__ = ['scraper_registry', 'OFACScraper', 'UNScraper']
