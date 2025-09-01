93eb1cd8be628aca39960473b8b4885baed6d15a
Mohammad Othman <emranm226@hotmail.com>
2025-09-01 12:23:48 +0300

    feat: Add security module output values

---
+++ infrastructure/modules/security/outputs.tf
@@ -0,0 +1,29 @@
+output "alb_security_group_id" {
+  description = "ALB security group ID"
+  value       = aws_security_group.alb.id
+}
+
+output "ec2_security_group_id" {
+  description = "EC2 security group ID"
+  value       = aws_security_group.ec2.id
+}
+
+output "rds_security_group_id" {
+  description = "RDS security group ID"
+  value       = aws_security_group.rds.id
+}
+
+output "redis_security_group_id" {
+  description = "Redis security group ID"
+  value       = aws_security_group.redis.id
+}
+
+output "ec2_iam_role_arn" {
+  description = "EC2 IAM role ARN"
+  value       = aws_iam_role.ec2.arn
+}
+
+output "ec2_instance_profile_name" {
+  description = "EC2 instance profile name"
+  value       = aws_iam_instance_profile.ec2.name
+}
