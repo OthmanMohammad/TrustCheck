dec227b671a93bf9db5c2bc0e32324b129929b9d
Mohammad Othman <emranm226@hotmail.com>
2025-09-01 12:24:06 +0300

    feat: Add database module input variables

---
+++ infrastructure/modules/database/variables.tf
@@ -0,0 +1,62 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "Database instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs for DB subnet group"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID for database"
+  type        = string
+}
+
+variable "backup_retention_period" {
+  description = "Backup retention in days"
+  type        = number
+  default     = 7
+}
+
+variable "deletion_protection" {
+  description = "Enable deletion protection"
+  type        = bool
+  default     = false
+}
+
+variable "create_read_replica" {
+  description = "Create read replica"
+  type        = bool
+  default     = false
+}
+
+variable "kms_key_id" {
+  description = "KMS key ID for encryption"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply"
+  type        = map(string)
+  default     = {}
+}
